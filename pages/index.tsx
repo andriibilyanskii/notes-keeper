097ef5bf4f635d48f984e81cca9409eea5c5d344
andriibilyanskii <andriibilyanskii@gmail.com>
2024-02-06 01:25:59 +0200

    main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
-import type { NextPage } from 'next';
+import type {NextPage} from 'next';
+import {useSession} from "next-auth/react";
 
 
-import { About, NotesPage } from '@components';
+import {About, NotesPage} from '@components';
 
-import { useUserContext } from '@shared';
+import {useUserContext} from '@shared';
 
 const Home: NextPage = () => {
-	const { user } = useUserContext();
+	const {user} = useUserContext();
 
-	// if (JSON.stringify(user) === '{}') {
-	// 	return <Loader manualShow={true} />;
-	// }
+	const {data: session} = useSession();
 
-	if (!user?._id) {
-		return <About />;
+
+	if (typeof session !== 'undefined' && !session?.user) {
+		return <About/>;
 	}
 
-	return <NotesPage />;
+	return <NotesPage/>;
 };
 
 export default Home;
